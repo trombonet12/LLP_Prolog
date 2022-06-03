64f2ef7d4e62af2a960148c6b91521a725c9ba1a
Joan López Ferrer <trombonet12@gmail.com>
2022-06-03 16:50:41 +0200

    paraula2 implementat

--- practica.pl
+++ practica.pl
@@ -14,6 +14,8 @@ mostra([],_,_,_).
 mostra([L|R],X,Y,horitzontal):-gotoXY(X,Y),escriu(L,blau),Y1 is Y+3,mostra(R,X,Y1,horitzontal).
 mostra([L|R],X,Y,vertical):-gotoXY(X,Y),escriu(L,vermell),X1 is X+1,mostra(R,X1,Y,vertical).
 
+paraula2(X):-paraula(_,_,Y,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(Y,X).
+
 creuats():-
     cls,
     paraula(H1),length(H1,7),paraula(V6),length(V6,11),
@@ -75,41 +77,41 @@ creuats():-
 
 creuats2():-
     cls,
-    paraula(_,_,H1L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(H1L,H1),length(H1,7),paraula(_,_,V6L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(V6L,V6),length(V6,11),
+    paraula2(H1),length(H1,7),paraula2(V6),length(V6,11),
     nth0(4,H1,C1),nth0(0,V6,C1),
 
-    paraula(_,_,H2L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(H2L,H2),length(H2,5),
+    paraula2(H2),length(H2,5),
     nth0(2,H2,C2),nth0(2,V6,C2),
 
-    paraula(_,_,H3L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(H3L,H3),length(H3,6),paraula(_,_,V1L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(V1L,V1),length(V1,5),
+    paraula2(H3),length(H3,6),paraula2(V1),length(V1,5),
     nth0(0,H3,C3),nth0(0,V1,C3),
 
-    paraula(_,_,V3L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(V3L,V3),length(V3,8),
+    paraula2(V3),length(V3,8),
     nth0(5,H3,C4),nth0(2,V3,C4),
 
-    paraula(_,_,H4L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(H4L,H4),length(H4,8),paraula(_,_,V2L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(V2L,V2),length(V2,8),
+    paraula2(H4),length(H4,8),paraula2(V2),length(V2,8),
     nth0(0,H4,C5),nth0(0,V2,C5),
 
     nth0(3,H4,C6),nth0(4,V3,C6),
 
-    paraula(_,_,V4L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(V4L,V4),length(V4,6),
+    paraula2(V4),length(V4,6),
     nth0(5,H4,C7),nth0(3,V4,C7),
 
-    paraula(_,_,V5L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(V5L,V5),length(V5,8),
+    paraula2(V5),length(V5,8),
     nth0(7,H4,C8),nth0(0,V5,C8),
 
-    paraula(_,_,H5L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(H5L,H5),length(H5,5),
+    paraula2(H5),length(H5,5),
     nth0(1,H5,C9),nth0(5,V2,C9),
 
-    paraula(_,_,H6L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(H6L,H6),length(H6,7),
+    paraula2(H6),length(H6,7),
     nth0(2,H6,C10),nth0(5,V5,C10),
 
     nth0(4,H6,C11),nth0(10,V6,C11),
 
-    paraula(_,_,V7L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(V7L,V7),length(V7,3),
+    paraula2(V7),length(V7,3),
     nth0(6,H6,C12),nth0(2,V7,C12),
 
-    paraula(_,_,H7L,_,_,_,_,_,_,_,_,_,_,_,_),atom_chars(H7L,H7),length(H7,10),
+    paraula2(H7),length(H7,10),
     nth0(5,H7,C13),nth0(7,V5,C13),
 
     \+repetides([H1,H2,H3,H4,H5,H6,H7,V1,V2,V3,V4,V5,V6,V7]),
